87bc6cb988f0d6045190c52cbc5e90248e9b8c4f
Zoe Dechand <ZoeDechand@zoes-macbook-air.local>
2019-05-09 20:17:05 +0200

    jawoll

--- eos/ViewController.swift
+++ eos/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var labl: UILabel!
     @IBAction func buttonClicked(_ sender: UIButton) {
         print("This is a Git tutorial")
-        labl.text = "I bims zoe ich bin drin 2"
+        labl.text = "I bims zoe ich bin drin 3"
     }
 
 
